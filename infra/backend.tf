34fa2fdd4695fe4630c96e453b00916dc6bcbe78
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-03-06 21:22:36 -0300

    adding infra

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3"{
-    bucket = "terraform-state-bucket-johnnycash"
-    region = "us-east-1"
-    encrypt = true 
+    bucket = ""
+    key = 
+    encrypt = true
   }
 }
--- infra/ecr.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ecr_repository" "ecr"{
-  name = "ecr-repo"
-  region = "us-east-1"
-}
